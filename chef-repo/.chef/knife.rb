413f63061f7f262fec2803ce5afd920f8fe9a6ad
sukeshss <sukeshss@gmail.com>
2014-06-19 16:38:01 +0530

    changed cookbooks path

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -1,1 +1,1 @@
-cookbook_path [ '/home/qburst/chef-solo-demo/chef-repo/cookbooks' ]
+cookbook_path [ '/root/chef-solo-demo/chef-repo/cookbooks' ]
--- chef-repo/solo.rb
+++ chef-repo/solo.rb
@@ -1,2 +1,2 @@
 file_cache_path "/root/chef-solo"
-cookbook_path "/home/qburst/chef-solo-demo/chef-repo/cookbooks"
+cookbook_path "/chef-solo-demo/chef-repo/cookbooks"
