20b55bb41a93c9549b76b3589bff5c12f8cc8ac2
sukeshss <sukeshss@gmail.com>
2014-06-20 15:10:28 +0530

    adding mysql cookbook

--- chef-repo/cookbooks/phpapp/recipes/default.rb
+++ chef-repo/cookbooks/phpapp/recipes/default.rb
@@ -7,7 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "apache2"
-
+include_recipe "mysql::client"
+include_recipe "mysql::server"
 apache_site "default" do
   enable true
 end
